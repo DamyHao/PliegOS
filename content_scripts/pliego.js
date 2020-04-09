f074216a6bb53952de39429911f7af15f4858fad
damia <d.casas@asdt.eu>
2020-04-09 12:01:18 +0200

    first commit

---
+++ content_scripts/pliego.js
@@ -0,0 +1,45 @@
+
+(function () {
+    /**
+     * Script injectat a la pagina.
+     */
+
+    /**
+   * Ens assegurem de no tenir mes de una vegada el mateix script corrent
+   */
+    if (window.hasRun) {
+        return;
+    }
+    //Variable global
+    window.hasRun = true;
+
+
+    function getInfo() {
+        //Obtenim tots els paragraphs
+
+        try {
+            //Utilitzant la llibreria readeable
+            var documentClone = document.cloneNode(true);
+            var article = new Readability(documentClone).parse();
+
+            var sending = browser.runtime.sendMessage({
+                article: article
+            });
+            sending.then(() => { console.log('FET') }).catch((err) => console.error(err));
+            //sending
+        }
+        catch (err) {
+            console.error(err)
+        }
+
+    }
+
+    //Missatges rebuts del js del popup
+    browser.runtime.onMessage.addListener((message) => {
+        console.log(message)
+        if (message.command === "getInfo") {
+            getInfo();
+        }
+
+    });
+})();
